cbe45bf181d5b9dbc9e5ad0296c4d6dc994432c5
powermad80 <hannahdubusker@proton.me>
2024-06-22 18:24:19 -0500

    slight progress in structure

--- main.cpp
+++ main.cpp
@@ -1,48 +1,50 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 class Reveal {
-  public:
-    int red;
-    int blue;
-    int green;
+public:
+  int red;
+  int blue;
+  int green;
 };
 
 class Game {
-  public:
-    int Id;
-    std::vector<Reveal> reveals;
+public:
+  int Id;
+  std::vector<Reveal> reveals;
 };
 
-Reveal parseGame(std::string game) {
+Reveal parseGame(std::string line) {
   std::istringstream iss(line);
   std::string word;
   Reveal rev;
   while (iss >> word) {
-    if (word == "Red")
-    {
-      iss >> rev.Red;
+    if (word == "Red") {
+      iss >> rev.red;
     }
   }
 }
 
-Game getAllGames() {
+std::vector<Game> getAllGames() {
   std::ifstream file("input.txt");
   std::string line;
-  Reveal rev;
+  Game game;
+  std::vector<Game> games;
+  std::vector<Reveal> reveals;
   while (std::getline(file, line)) {
-    rev = parseGame(line);
+    game = parseGame(line);
+    games.push_back(game);
   }
 }
 
 int main() {
-    std::ifstream file("input.txt");
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-    }
-    return 0;
+  std::ifstream file("input.txt");
+  std::string line;
+  while (std::getline(file, line)) {
+    std::cout << line << std::endl;
+  }
+  return 0;
 }
