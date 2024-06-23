97e15051d1b4417688723f6f8f33309e3ec71221
powermad80 <hannahdubusker@proton.me>
2024-06-22 23:08:25 -0500

    structure complete, but returns wrong answer

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -17,15 +19,80 @@ public:
   std::vector<Reveal> reveals;
 };
 
-Reveal parseGame(std::string line) {
-  std::istringstream iss(line);
-  std::string word;
-  Reveal rev;
-  while (iss >> word) {
-    if (word == "Red") {
-      iss >> rev.red;
+static inline void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }));
+}
+
+static inline void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       [](unsigned char ch) { return !std::isspace(ch); })
+              .base(),
+          s.end());
+}
+
+static inline void trim(std::string &s) {
+  ltrim(s);
+  rtrim(s);
+}
+
+std::vector<std::string> split(const std::string &s, char delimiter) {
+  std::vector<std::string> substrings;
+  std::string substring;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, substring, delimiter)) {
+    substrings.push_back(substring);
+  }
+  return substrings;
+}
+
+int getGameId(std::string line) {
+  std::vector<std::string> partedLine = split(line, ' ');
+  std::string idString = partedLine[1];
+  trim(idString);
+  return stoi(idString);
+}
+
+Reveal parseRevealString(std::string line) {
+  trim(line);
+  std::vector<std::string> partedLine = split(line, ' ');
+  int dice;
+  char color;
+  Reveal reveal;
+  for (int i = 0; i < partedLine.size(); i = i + 2) {
+    dice = stoi(partedLine[i]);
+    color = partedLine[i + 1][0];
+    if (color == 'r') {
+      reveal.red = dice;
+    } else if (color == 'g') {
+      reveal.green = dice;
+    } else if (color == 'b') {
+      reveal.blue = dice;
     }
   }
+  return reveal;
+}
+
+std::vector<Reveal> getGameReveals(std::string line) {
+  std::vector<Reveal> reveals;
+  Reveal reveal;
+  std::vector<std::string> revealsString = split(line, ';');
+  for (int i = 0; i < revealsString.size(); i++) {
+    reveal = parseRevealString(revealsString[i]);
+    reveals.push_back(reveal);
+  }
+  return reveals;
+}
+
+Game parseGame(std::string line) {
+  std::vector<std::string> partedLine = split(line, ':');
+  std::string gameIdString = partedLine[0];
+  std::string revealsString = partedLine[1];
+  Game game;
+  game.Id = getGameId(gameIdString);
+  game.reveals = getGameReveals(revealsString);
+  return game;
 }
 
 std::vector<Game> getAllGames() {
@@ -38,13 +105,34 @@ std::vector<Game> getAllGames() {
     game = parseGame(line);
     games.push_back(game);
   }
+  return games;
+}
+
+bool isGamePossible(Game game) {
+  // Set dice limits here
+  int maxRed = 12;
+  int maxGreen = 13;
+  int maxBlue = 14;
+  Reveal rev;
+  for (int i = 0; i < game.reveals.size(); i++) {
+    rev = game.reveals[i];
+    if (rev.red > maxRed || rev.blue > maxBlue || rev.green > maxGreen) {
+      return false;
+    }
+  }
+  return true;
 }
 
 int main() {
-  std::ifstream file("input.txt");
-  std::string line;
-  while (std::getline(file, line)) {
-    std::cout << line << std::endl;
+  std::vector games = getAllGames();
+  Game game;
+  int total = 0;
+  for (int i = 0; i < games.size(); i++) {
+    game = games[i];
+    if (isGamePossible(game)) {
+      total += game.Id;
+    }
   }
-  return 0;
+  std::cout << total;
+  return total;
 }
